f846fce80eeff95da86525e32ee095ecb8bb3bce
William Chen <wlmchen16@gmail.com>
2021-10-26 15:22:49 -0700

    comment out the modal

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -63,11 +63,11 @@ function HeroHome() {
             </div>
 
             {/* Modal */}
-            <Modal id="modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={() => setVideoModalOpen(false)}>
+            {/* <Modal id="modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={() => setVideoModalOpen(false)}>
               <div className="relative pb-9/16">
                 <iframe className="absolute w-full h-full" src="https://player.vimeo.com/video/174002812" title="Video" allowFullScreen></iframe>
               </div>
-            </Modal>
+            </Modal> */}
 
           </div>
           
