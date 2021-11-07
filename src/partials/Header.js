b7e6280a6e64bf61c9f32edec99c107398bc27c6
William Chen <wlmchen16@gmail.com>
2021-11-07 14:37:18 -0800

    resposive header

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -6,6 +6,7 @@ import Dropdown from '../utils/Dropdown';
 function Header() {
 
   const [top, setTop] = useState(true);
+  const [navbarOpen, setNavbarOpen] = useState(false);
 
   // detect whether user has scrolled the page down by 10px 
   useEffect(() => {
@@ -17,21 +18,34 @@ function Header() {
   }, [top]);  
 
   return (
-    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white blur shadow-lg'}`}>
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20">
+    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out bg-white ${!top && 'bg-white blur shadow-lg'}`}>
+      <nav className="max-w-6xl mx-auto px-5 sm:px-6">
+        <div className="flex flex-wrap items-center justify-between py-2 md:py-4">
 
           {/* Site branding */}
-          <div className="flex-shrink-0 mr-4">
+          <div className="w-full relative flex justify-between md:w-auto md:static md:block md:justify-start">
+
+          {/* <div className="w-full flex-grow flex-row justify-between md:w-auto mr-4"> !!!!!! mr-4 */}
             {/* Logo */}
             <Link to="/" className="block" aria-label="Tech Support">
               <img src={require('../images/logos/7419_light.png').default} width='40px' alt="7419 Logo" />
             </Link>
+            <button onClick={() => setNavbarOpen(!navbarOpen)} className="focus:outline-none leading-none md:hidden">
+              <svg className="text-gray-800 hover:text-blue-600" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+            </button>
           </div>
 
           {/* Site navigation */}
-          <nav className="flex flex-grow">
-            <ul className="flex flex-grow justify-end flex-wrap items-center">
+          <div
+            className={
+              "md:flex flex-grow items-center transition-opacity" +
+              (navbarOpen ? " flex" : " hidden") // TODO: make this transition
+            }
+          >
+          
+            <ul className="flex flex-grow flex-col md:flex-row md:justify-end flex-wrap items-center">
               <li>
                 <Link to="/" className="font-medium text-gray-600 hover:text-gray-800 px-5 py-3 flex items-center transition duration-150 ease-in-out">
                   Home
@@ -60,10 +74,10 @@ function Header() {
               </li>
             </ul>
 
-          </nav>
+          </div>
 
         </div>
-      </div>
+      </nav>
     </header>
   );
 }
