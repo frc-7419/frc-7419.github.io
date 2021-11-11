242667a33ba356611898240aa082502051b8eb50
pryme-svg <edoc.www@gmail.com>
2021-11-11 13:47:55 -0800

    CRA -> Next.js

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import HeroHome from '../partials/HeroHome';
+import FeaturesHome from '../partials/Features';
+import FeaturesBlocks from '../partials/FeaturesBlocks';
+import Newsletter from '../partials/Newsletter';
+
+function Home() {
+  return (
+
+      <main className="flex-grow">
+
+        {/*  Page sections */}
+        <HeroHome />
+
+        <FeaturesHome />
+        <FeaturesBlocks />
+        <Newsletter />
+
+      </main>
+  );
+}
+
+export default Home;
