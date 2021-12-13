37e8c46f36249727134c4e5fbeb120dfb7cde290
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-12-12 16:39:11 -0800

    Revert "Revert "Merge branch 'nextjs'""
    This reverts commit b35e9d2d20afac81ea666e7b84d64eae819ad453.

--- utils/Layout.js
+++ utils/Layout.js
@@ -1,9 +1,8 @@
 import Header from '../partials/Header';
-import Footer from '../partials/Footer';
+import FooterTemp from '../partials/FooterTemp';
 
 import Head from 'next/head'
 
-import 'animate.css';
 
 export default function Layout({ children }) {
   return (
@@ -15,11 +14,15 @@ export default function Layout({ children }) {
         <meta name="theme-color" content="#000000" />
         <meta
         name="description"
-        content="7419 Tech Support"
+        content="Official website for FRC 7419 Team"
         />
         <link rel="apple-touch-icon" href="/logo192.png" />
 
         <link rel="manifest" href="/manifest.json" />
+        <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
 
         <title>7419 Tech Support</title>
       </Head>
@@ -28,8 +31,10 @@ export default function Layout({ children }) {
         <div id="root" className="font-inter antialiased bg-white text-gray-900 tracking-tight">
           <div className="flex flex-col min-h-screen overflow-hidden">
               {children}
-              <Footer />
+              <FooterTemp />
+
           </div>
+
         </div>
     </>
   )
