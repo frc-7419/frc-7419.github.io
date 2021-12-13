e93d3b6cef26bcb67da9199abae53db5aa0f831b
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2021-12-12 16:42:41 -0800

    Fix team.js merge issue

--- pages/about/team.js
+++ pages/about/team.js
@@ -1,43 +1,25 @@
-import React from 'react';
-
 function Team() {
   return (
-      <main className="flex-grow">
-        <section className="relative">
-        
-          {/* Illustration behind content */}
-          <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -mb-32" aria-hidden="true">
-            <svg width="1760" height="518" viewBox="0 0 1760 518" xmlns="http://www.w3.org/2000/svg">
-              <defs>
-                <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-02">
-                  <stop stopColor="#FFF" offset="0%" />
-                  <stop stopColor="#EAEAEA" offset="77.402%" />
-                  <stop stopColor="#DFDFDF" offset="100%" />
-                </linearGradient>
-              </defs>
-              <g transform="translate(0 -3)" fill="url(#illustration-02)" fillRule="evenodd">
-                <circle cx="1630" cy="128" r="128" />
-                <circle cx="178" cy="481" r="40" />
-              </g>
-            </svg>
-          </div>
+    <main className="flex-grow">
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="py-12 md:py-20">
 
-          <div className="max-w-6xl mx-auto px-4 sm:px-6">
-            <div className="py-12 md:py-20">
+            {/* Section header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-14">
+              <h2 className="h2">Our Story</h2>
+              {/* <p className="text-xl text-gray-600" data-aos="zoom-y-out"></p> */}
+            </div>
 
-              {/* Section header */}
-              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-                <h2 className="h2 mb-4">Our Team</h2>
-                <p className="text-xl text-gray-600" data-aos="zoom-y-out">Coming soon...</p>
-              </div>
-
-
+            {/* Items */}
+            <div className="max-w-sm md:max-w-4xl mx-auto grid">
+              <p className="text-gray-800">Our FRC team has grown out of a small FTC program that has been with the FIRST community for many years - FTC 5214 - Tech Support.  Due to graduating members, we experienced a “soft restart” in 2015, our team was left entirely with rookies who struggled to even turn the robot on. Since the 2015 season, our team has rebuilt itself, found new mentorship, and grown in size dramatically. We gained expertise by building community ties and relying on other FTC teams for advice, and expanded our program through our outreach events and forming bonds with new people. In 2018, we had a stellar season and were able to win the Northern California FTC Championship. We earned a spot at the Houston championship, and were especially happy that we were able to finish in the top fourth of all the teams that competed at Worlds. While participating at the Championship, we were exposed to two different environments: FRC and FTC. After returning from Houston, we had a team discussion and we decided that we would try to participate in FRC. We were able to convince our school’s administration to support us in our mission, we have recruited many new students, more technical mentors, and forged partnerships with local companies. We’re excited for our first FRC season and look forward to what the future will bring.</p>
             </div>
           </div>
-        </section>
-      </main>
-
-  );
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default Team;
