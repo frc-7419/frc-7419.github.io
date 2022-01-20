a68328bef49cd5b153c1ab60d619f3cd029cf2d4
Siddharth Srinivasan <77374762+H0tP0cket@users.noreply.github.com>
2022-01-20 14:30:54 -0800

    image slider test 2

--- pages/about/robot.js
+++ pages/about/robot.js
@@ -3,6 +3,48 @@ import React from 'react';
 
 
     
+class ImageSlider extends Component {
+
+    state = {
+        image: [
+            "https://cdn.britannica.com/68/143568-050-5246474F/Donkey.jpg",
+            "https://cdn.mos.cms.futurecdn.net/6h8C6ygTdR2jyyUxkALwsc.jpg",
+            "https://media.istockphoto.com/photos/donkey-picture-id93216008?k=20&m=93216008&s=612x612&w=0&h=ufFslrWtjVYHgIVtV4UYUeO5x2dMOswZvwFSsK4RJU8=",
+            "https://upload.wikimedia.org/wikipedia/commons/1/1a/Donkey_in_Clovelly%2C_North_Devon%2C_England.jpg"
+        ],
+        idx: 0
+    };
+
+    next = () => {
+      console.log (this.state);
+      this.setState({
+        idx: this.state.idx +1
+      });
+    };
+    previous = () => {
+      console.log (this.state);
+      this.setState({
+        idx: this.state.idx -1
+      });
+    };
+    render() {
+      return (
+        <div>
+          <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+           onClick={this.previous}>{'<'}</button>
+          <img alt=""
+          style={{ 
+            width: 500,
+            height: 500,
+          }}
+          src = {this.state.image[this.state.idx]} />
+          <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+           onClick={this.next}>{'>'}</button>
+        </div>
+
+      );
+    }
+  }
 
 function Robot() {
         return (
@@ -19,22 +61,7 @@ function Robot() {
                     </p>
                 </div>
                 {/* 2020 Robot */}
-                render() {
-                    <div className="main">
-                        <h1 className="title">Robo Pictures</h1>
-                        <div className="wrapper">
-                            <a className="prev" onClick={this.scroll.bind(null,-1)}>&#10094;</a>
-                            <div className="image-container">
-                            <div className="image">1</div>
-                            <div className="image">2</div>
-                            <div className="image">3</div>
-                            <div className="image">4</div>
-                            <div className="image">5</div>
-                        </div>
-                        <a className="next" onClick={this.scroll.bind(null,1)}>&#10095;</a>
-                    </div>
-                </div>
-    };
+                <ImageSlider />
                 {/* Items */}
                 <div className="max-w-sm md:max-w-4xl mx-auto grid">
                     <p className="text-gray-800">
