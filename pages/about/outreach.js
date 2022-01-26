e7bdbe8db9542746e3f7b16b66606cba55e240a8
pryme-svg <edoc.www@gmail.com>
2022-01-25 17:25:03 -0800

    kindness week

--- pages/about/outreach.js
+++ pages/about/outreach.js
@@ -34,58 +34,64 @@ function Outreach() {
 
               {/* Items */}
               <div className="max-w-sm md:max-w-4xl mx-auto grid">
-                <div>
-                  <h3 className="h3 mb-4">Outreach Events</h3>
-                  <ul>
-                    <li>
-                      <div>
-                        <h4 className="h4 mb-3">Lego Mindstorm</h4>
-                        <ul>
-                          <li className="mb-3">
-                            <p className="text-gray-600">The kids are introduced to analytical problem solving and creative thinking in a team setting. By middle school, they are invited to compete in the First Tech Challenge with Team 14770.
-                        High school students regularly mentor their progress and provide valuable instruction based on prior experience.</p>
-                          </li>
-                          <li className="mb-3">
-                            <p className="text-gray-600">In high school, we have 3 FTC teams to train new members and give them a chance to decide what they want out of engaging in the robotics team. We teach them skills of CAD or programming, utilizing 3D printer, laser cutter, and CNC mill.</p>
-                          </li>
-                          <li className="mb-3">
-                            <p className="text-gray-600">Our hope is that they are not only left with a little more technological intuition than an hour ago but are curious about the ingenious culture of FIRST robotics that our members come from.</p>
-                          </li>
-                        </ul>
-                      </div>
-                    </li>
-                    <li>
-                      <div>
-                        <h4 className="h4 mb-3">Genius Kids</h4>
-                        <ul>
-                          <li className="mb-3">
-                            <p className="text-gray-600">It is an after-school enrichment program to present scientific and engineering concepts to young children in a captivating manner to open their eyes to the possibilities right at their fingertips! Our hope is that they are not only left with a little more technological intuition than an hour ago but are curious about the ingenious culture of FIRST robotics that our members come from. Our hope is that they are not only left with a little more technological intuition than an hour ago but are curious about the ingenious culture of FIRST robotics that our members come from.</p>
-                          </li>
-                        </ul>
-                      </div>
-                    </li>
-                    <li>
-                      <div>
-                        <h4 className="h4 mb-3">Hosted FLL and FTC tournaments</h4>
-                        <ul>
-                          <li className="mb-3">
-                            <p className="text-gray-600">We hosted 2 tournaments this year in order to give more teams an opportunity to compete, and to help strengthen FIRST’s presence in the tri-valley area. We hosted and ran both an FLL and an FTC tournament, with many of 7419 members helping during planning, organization, and volunteering at the event.</p>
-                          </li>
-                        </ul>
-                      </div>
-                    </li>
-                    <li>
-                      <div>
-                        <h4 className="h4 mb-3">Assisting FTC teams</h4>
-                        <ul>
-                          <li className="mb-3">
-                            <p className="text-gray-600">We assist FTC teams at other tournaments and donate them parts and electronics, and have volunteered to help out at the FTC state championships with setup, and provided referees and inspectors, and donated our practice field for the past 2 years.</p>
-                          </li>
-                        </ul>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
+                <p className="text-gray-600 mb-4">A significant part of the FIRST experience heavily revolves around outreach. Outreach not only emphasizes the impact and benefits of robotics but also inspires people of all ages to pursue an interest in STEM. In our team, every student makes a commitment to participate in outreach, with our activities spanning from assisting FLL and FTC teams to holding workshops at after school programs like Genius Kids. Here are just a few examples of outreach events we’ve held:</p>
+                <ul>
+                  <li className="mb-3">
+                    <h4 className="h4 mb-3">2021 Kindness Week</h4>
+                    <p className="text-gray-600 mb-4">During this week, we donated to shelters, sent letters to healthcare workers, and 3D printed toys. As a team that is part of the FRC community, we not only learn about STEM, but also recognize how crucial it is to help others and give back to those who made sacrifices during the pandemic to assist the sick.</p>
+                    <div className='grid grid-cols-1'>
+                      <img className="rounded mb-4 mx-auto max-w-xl" src="/static/images/outreach/2021_kindness_week/letters.jpg" alt="Letters" />
+                      <img className="rounded mb-4 mx-auto max-w-lg" src="/static/images/outreach/2021_kindness_week/toilet_paper.png" alt="Toilet Paper" />
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <h4 className="h4 mb-3">Lego Mindstorm</h4>
+                      <ul>
+                        <li className="mb-3">
+                          <p className="text-gray-600">The kids are introduced to analytical problem solving and creative thinking in a team setting. By middle school, they are invited to compete in the First Tech Challenge with Team 14770.
+                      High school students regularly mentor their progress and provide valuable instruction based on prior experience.</p>
+                        </li>
+                        <li className="mb-3">
+                          <p className="text-gray-600">In high school, we have 3 FTC teams to train new members and give them a chance to decide what they want out of engaging in the robotics team. We teach them skills of CAD or programming, utilizing 3D printer, laser cutter, and CNC mill.</p>
+                        </li>
+                        <li className="mb-3">
+                          <p className="text-gray-600">Our hope is that they are not only left with a little more technological intuition than an hour ago but are curious about the ingenious culture of FIRST robotics that our members come from.</p>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <h4 className="h4 mb-3">Genius Kids</h4>
+                      <ul>
+                        <li className="mb-3">
+                          <p className="text-gray-600">It is an after-school enrichment program to present scientific and engineering concepts to young children in a captivating manner to open their eyes to the possibilities right at their fingertips! Our hope is that they are not only left with a little more technological intuition than an hour ago but are curious about the ingenious culture of FIRST robotics that our members come from. Our hope is that they are not only left with a little more technological intuition than an hour ago but are curious about the ingenious culture of FIRST robotics that our members come from.</p>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <h4 className="h4 mb-3">Hosted FLL and FTC tournaments</h4>
+                      <ul>
+                        <li className="mb-3">
+                          <p className="text-gray-600">We hosted 2 tournaments this year in order to give more teams an opportunity to compete, and to help strengthen FIRST’s presence in the tri-valley area. We hosted and ran both an FLL and an FTC tournament, with many of 7419 members helping during planning, organization, and volunteering at the event.</p>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                  <li>
+                    <div>
+                      <h4 className="h4 mb-3">Assisting FTC teams</h4>
+                      <ul>
+                        <li className="mb-3">
+                          <p className="text-gray-600">We assist FTC teams at other tournaments and donate them parts and electronics, and have volunteered to help out at the FTC state championships with setup, and provided referees and inspectors, and donated our practice field for the past 2 years.</p>
+                        </li>
+                      </ul>
+                    </div>
+                  </li>
+                </ul>
               </div>
 
             </div>
