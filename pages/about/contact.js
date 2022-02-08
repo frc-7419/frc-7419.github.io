9d2d8450f8a0ac2d98a09451f676e9ec072fc2ad
Siddharth Srinivasan <77374762+H0tP0cket@users.noreply.github.com>
2022-02-07 23:44:33 -0800

    Updated contact.js logic

--- pages/about/contact.js
+++ pages/about/contact.js
@@ -1,8 +1,36 @@
 import React from 'react';
 
-class ContactUs extends React.Component {
+import { useState } from "react";
+
+
+const ContactUs = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [team, setTeam] = useState('');
+  const [msg, setMsg] = useState('');
+  const [ispending, setIsPending] = useState(false);
   
-    render() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = {name, email, phone, team, msg}
+
+    setIsPending(true);
+
+    fetch('http://localhost:3000/about/contact', { //change to 7419.tech before pushing to main
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(form)
+    }).then(() => {
+      console.log("form submitted:", form)
+      setIsPending(false);
+      
+    }
+    )
+  }
+  
+    
       return (
         <main className="flex-grow">
       <section className="relative">
@@ -20,25 +48,29 @@ class ContactUs extends React.Component {
                 {/* <h4 className="h4 mb-3">Header</h4> */}
                 Contact Us for questions? idk
               </p>
-              <form>
-                <input class=" text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" id="name" name="name" 
+              <form onSubmit={handleSubmit}>
+                <input value ={name} onChange={(e) => setName(e.target.value)} class=" text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" required id="name" name="name" 
                 placeholder="Enter Name" onfocus="this.placeholder=''" onblur="this.placeholder='Name'"></input><br></br><br></br>
-                <input class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg" type="text" id="email" name="email" placeholder="Enter Email
+                <input value ={email} onChange={(e) => setEmail(e.target.value)} class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg" type="text" required  id="email" name="email" placeholder="Enter Email
                 " onfocus="this.placeholder=''" onblur="this.placeholder='Email'" ></input><br></br><br></br>
-                <input class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" id="phone" name="phone" 
+                <input value ={phone} onChange={(e) => setPhone(e.target.value)} class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" id="phone" name="phone" 
                 placeholder="Enter Phone #" onfocus="this.placeholder=''" 
                 onblur="this.placeholder='Phone #'" ></input><br></br><br></br>
-                <input class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" id="team" name="team" 
+                <input value ={team} onChange={(e) => setTeam(e.target.value)} class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" id="team" name="team" 
                 placeholder="Enter Team #" onfocus="this.placeholder=''" 
                 onblur="this.placeholder='Message'" ></input><br></br><br></br>
-                <textarea class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg" name="your-message" cols="40" rows="10"  aria-required="true" aria-invalid="false" spellcheck="false" placeholder="Enter Message"></textarea>
+                <textarea value ={msg} onChange={(e) => setMsg(e.target.value)} class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg" name="your-message" cols="40" rows="10"  aria-required="true" aria-invalid="false" spellcheck="false" placeholder="Enter Message" required></textarea>
             
                 
                 <br></br>
-                <button class="flex justify-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                { !ispending && <button class="flex justify-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
                   Submit
-                </button>
+                </button>}
+                { ispending && <button class="flex justify-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded opacity-50 cursor-not-allowed">
+                  Submitting
+                </button>}
               </form>
+              
             </div>
             
           </div>
@@ -47,6 +79,6 @@ class ContactUs extends React.Component {
     </main>
       );
     }
-  }
+  
   
   export default ContactUs;
