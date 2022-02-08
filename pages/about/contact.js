1484ea22e90e6f45fab982c0a7dc67e17ca5671c
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2022-02-08 10:10:52 -0800

    fix bugs

--- pages/about/contact.js
+++ pages/about/contact.js
@@ -49,24 +49,24 @@ const ContactUs = () => {
                 Contact Us for questions? idk
               </p>
               <form onSubmit={handleSubmit}>
-                <input value ={name} onChange={(e) => setName(e.target.value)} class=" text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" required id="name" name="name" 
-                placeholder="Enter Name" onfocus="this.placeholder=''" onblur="this.placeholder='Name'"></input><br></br><br></br>
-                <input value ={email} onChange={(e) => setEmail(e.target.value)} class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg" type="text" required  id="email" name="email" placeholder="Enter Email
-                " onfocus="this.placeholder=''" onblur="this.placeholder='Email'" ></input><br></br><br></br>
-                <input value ={phone} onChange={(e) => setPhone(e.target.value)} class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" id="phone" name="phone" 
+                <input value ={name} onChange={(e) => setName(e.target.value)} className=" text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" required id="name" name="name" 
+                placeholder="Enter Name" onfocus="this.placeholder=''" onBlur="this.placeholder='Name'"></input><br></br><br></br>
+                <input value ={email} onChange={(e) => setEmail(e.target.value)} className="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg" type="text" required  id="email" name="email" placeholder="Enter Email
+                " onfocus="this.placeholder=''" onBlur="this.placeholder='Email'" ></input><br></br><br></br>
+                <input value ={phone} onChange={(e) => setPhone(e.target.value)} className="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" id="phone" name="phone" 
                 placeholder="Enter Phone #" onfocus="this.placeholder=''" 
-                onblur="this.placeholder='Phone #'" ></input><br></br><br></br>
-                <input value ={team} onChange={(e) => setTeam(e.target.value)} class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" id="team" name="team" 
+                onBlur="this.placeholder='Phone #'" ></input><br></br><br></br>
+                <input value ={team} onChange={(e) => setTeam(e.target.value)} className="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg"  type="text" id="team" name="team" 
                 placeholder="Enter Team #" onfocus="this.placeholder=''" 
-                onblur="this.placeholder='Message'" ></input><br></br><br></br>
-                <textarea value ={msg} onChange={(e) => setMsg(e.target.value)} class="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg" name="your-message" cols="40" rows="10"  aria-required="true" aria-invalid="false" spellcheck="false" placeholder="Enter Message" required></textarea>
+                onBlur="this.placeholder='Message'" ></input><br></br><br></br>
+                <textarea value ={msg} onChange={(e) => setMsg(e.target.value)} className="  text-gray-400 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white drop-shadow-lg" name="your-message" cols="40" rows="10"  aria-required="true" aria-invalid="false" spellcheck="false" placeholder="Enter Message" required></textarea>
             
                 
                 <br></br>
-                { !ispending && <button class="flex justify-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                { !ispending && <button className="flex justify-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
                   Submit
                 </button>}
-                { ispending && <button class="flex justify-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded opacity-50 cursor-not-allowed">
+                { ispending && <button className="flex justify-center bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded opacity-50 cursor-not-allowed">
                   Submitting
                 </button>}
               </form>
