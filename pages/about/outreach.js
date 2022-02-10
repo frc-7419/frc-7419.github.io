0e5542b7736f152cf33be330f66d8e62528d9e48
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2022-02-09 22:36:35 -0800

    Fix compatibility on mobile

--- pages/about/outreach.js
+++ pages/about/outreach.js
@@ -33,15 +33,15 @@ function Outreach() {
               </div>
 
               {/* Items */}
-              <div className="max-w-sm md:max-w-4xl mx-auto grid">
-                <p className="text-gray-600 mb-4">A significant part of the FIRST experience heavily revolves around outreach. Outreach not only emphasizes the impact and benefits of robotics but also inspires people of all ages to pursue an interest in STEM. In our team, every student makes a commitment to participate in outreach, with our activities spanning from assisting FLL and FTC teams to holding workshops at after school programs like Genius Kids. Here are just a few examples of outreach events we’ve held:</p>
+              <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                <p className="text-gray-600 mb-4 prose">A significant part of the FIRST experience heavily revolves around outreach. Outreach not only emphasizes the impact and benefits of robotics but also inspires people of all ages to pursue an interest in STEM. In our team, every student makes a commitment to participate in outreach, with our activities spanning from assisting FLL and FTC teams to holding workshops at after school programs like Genius Kids. Here are just a few examples of outreach events we’ve held:</p>
                 <ul>
                   <li className="mb-3">
                     <h4 className="h4 mb-3">2021 Kindness Week</h4>
-                    <p className="text-gray-600 mb-4">During this week, we donated to shelters, sent letters to healthcare workers, and 3D printed toys. As a team that is part of the FRC community, we not only learn about STEM, but also recognize how crucial it is to help others and give back to those who made sacrifices during the pandemic to assist the sick.</p>
+                    <p className="text-gray-600 mb-4 max-w-full ">During this week, we donated to shelters, sent letters to healthcare workers, and 3D printed toys. As a team that is part of the FRC community, we not only learn about STEM, but also recognize how crucial it is to help others and give back to those who made sacrifices during the pandemic to assist the sick.</p>
                     <div className='grid grid-cols-1'>
-                      <img className="rounded mb-4 mx-auto max-w-sm md:max-w-md xl:max-w-xl" src="/static/images/outreach/2021_kindness_week/letters.jpg" alt="Letters" />
-                      <img className="rounded mb-4 mx-auto max-w-sm md:max-w-md xl:max-w-xl" src="/static/images/outreach/2021_kindness_week/toilet_paper.png" alt="Toilet Paper" />
+                      <img className="rounded mb-4 mx-auto max-w-sm md:max-w-md xl:max-w-xl w-full" src="/static/images/outreach/2021_kindness_week/letters.jpg" alt="Letters" />
+                      <img className="rounded mb-4 mx-auto max-w-sm md:max-w-md xl:max-w-xl w-full" src="/static/images/outreach/2021_kindness_week/toilet_paper.png" alt="Toilet Paper" />
                     </div>
                   </li>
                   <li>
@@ -49,7 +49,7 @@ function Outreach() {
                       <h4 className="h4 mb-3">First Lego League and First Tech Challenge</h4> 
                       <p className="text-gray-600 mb-4">Along with our high school FRC team we also have a FTC middle school team. To help guide the middle schoolers, we regularly mentor their progress and provide valuable instruction based on prior experience. We provide any type of assistance we can offer, from teaching them essential skills such as CAD or programming, to utilizing the 3D printer, laser cutter, and CNC mill.</p>
                       <p className="text-gray-600 mb-4">Along with mentoring, we have hosted 2 tournaments in the past in order to give more teams an opportunity to compete, as well as to help strengthen FIRST’s presence in the tri-valley area. We hosted and ran both an FLL and an FTC tournament, with many of 7419 members helping during planning, organization, and volunteering at the event. </p>
-                      <img className="my-4 rounded max-w-sm md:max-w-md lg:max-w-lg" src="/static/images/outreach/fll.jpg" alt="FLL Tournament" />
+                      <img className="my-4 rounded max-w-sm md:max-w-md lg:max-w-lg w-full" src="/static/images/outreach/fll.jpg" alt="FLL Tournament" />
                     </div>
                   </li>  
                   <li>
