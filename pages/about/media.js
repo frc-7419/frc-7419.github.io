c38b9de78c197e6b3eee3d67adc4b4ecd71c3743
Siddharth Srinivasan <77374762+H0tP0cket@users.noreply.github.com>
2022-02-15 23:31:28 -0800

    ReactPlayer added

--- pages/about/media.js
+++ pages/about/media.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import ReactPlayer from 'react-player'
   function Outreach() {
   
   
@@ -19,9 +19,9 @@ import React, { useState } from 'react';
               </div>
 
               {/* Items */}
-          <div class="max-w-sm md:max-w-7xl mx-auto grid grid-cols-3 gap-4">
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src="/static/images/2020robot.png" alt="L" className="w-full"/>
+          <div class="max-w-sm md:max-w-7xl mx-auto grid grid-cols-3 gap-4 overflow-visible">
+             <div className="max-w-sm rounded shadow-lg">
+                <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
                   <div className="text-center px-6 py-4 font-bold  text-xl mb-2">Robot</div>
              </div>
              <div className="max-w-sm rounded overflow-hidden shadow-lg">
