e921ddaea3caa7386fb27f78785f5e150e39fdb3
Siddharth Srinivasan <sidsrinivasan6@gmail.com>
2022-02-22 13:31:30 -0800

    Update media.js

--- pages/about/media.js
+++ pages/about/media.js
@@ -1,64 +1,106 @@
-import React, { useState } from 'react';
-import ReactPlayer from 'react-player'
-  function Media() {
-  
-  
-  
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+function Media() {
   return (
     <main className="flex-grow">
-        <section className="relative">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6">
-            <div className="py-12 md:py-20">
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="py-12 md:py-20">
+            {/* Section header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+              <h2 className="h2 mb-4">Media</h2>
+              <p className="text-xl text-gray-600" data-aos="zoom-y-out">
+                A collection of media that represents our team
+              </p>
+            </div>
+          </div>
+        </div>
 
-              {/* Section header */}
-              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-                <h2 className="h2 mb-4">Media</h2>
-                <p className="text-xl text-gray-600" data-aos="zoom-y-out">A collection of media that represents our team</p>
-              </div>
-              </div>
-              </div>
-
-              {/* Items */}
-          <div className="max-w-sm md:max-w-7xl mx-auto grid grid-cols-3 gap-4 overflow-visible">
-             <div className="max-w-sm rounded shadow-lg">
-                <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-                  <div className="text-center px-6 py-4 font-bold  text-xl mb-2">Robot</div>
-             </div>
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src="/static/images/2020robot.png" alt="L" className="w-full"/>
-                  <div className="text-center px-6 py-4 font-bold  text-xl mb-2">Robot</div>
-             </div> 
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src="/static/images/2020robot.png" alt="L" className="w-full"/>
-                  <div className="text-center px-6 py-4 font-bold  text-xl mb-2">Robot</div>
-             </div> 
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src="/static/images/2020robot.png" alt="L" className="w-full"/>
-                  <div className="text-center px-6 py-4 font-bold  text-xl mb-2">Robot</div>
-             </div> 
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src="/static/images/2020robot.png" alt="L" className="w-full"/>
-                  <div className="text-center px-6 py-4 font-bold  text-xl mb-2">Robot</div>
-             </div> 
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src="/static/images/2020robot.png" alt="L" className="w-full"/>
-                  <div className="text-center px-6 py-4 font-bold  text-xl mb-2">Robot</div>
-             </div> 
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src="/static/images/2020robot.png" alt="L" className="w-full"/>
-                  <div className="text-center px-6 py-4 font-bold  text-xl mb-2">Robot</div>
-             </div>
-             <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img src="/static/images/2020robot.png" alt="L" className="w-full"/>
-                  <div className="text-center px-6 py-4 font-bold  text-xl mb-2">Robot</div>
-             </div>  
-          </div> 
-
-          
-             
-        </section>
-      </main>
-
+        {/* Items */}
+        <div className="max-w-sm md:max-w-7xl mx-auto grid grid-cols-3 gap-4 ">
+          <div className="bg-white max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="hover:overflow-x-scroll">
+            <ReactPlayer url="https://www.youtube.com/watch?v=YkSaKTfk0R4" />
+            </div>
+            <div className="text-center px-6 py-4 font-bold  text-xl mb-2">
+              Robot
+            </div>
+          </div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img
+              src="/static/images/2020robot.png"
+              alt="L"
+              className="w-full"
+            />
+            <div className="text-center px-6 py-4 font-bold  text-xl mb-2">
+              Robot
+            </div>
+          </div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img
+              src="/static/images/2020robot.png"
+              alt="L"
+              className="w-full"
+            />
+            <div className="text-center px-6 py-4 font-bold  text-xl mb-2">
+              Robot
+            </div>
+          </div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img
+              src="/static/images/2020robot.png"
+              alt="L"
+              className="w-full"
+            />
+            <div className="text-center px-6 py-4 font-bold  text-xl mb-2">
+              Robot
+            </div>
+          </div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img
+              src="/static/images/2020robot.png"
+              alt="L"
+              className="w-full"
+            />
+            <div className="text-center px-6 py-4 font-bold  text-xl mb-2">
+              Robot
+            </div>
+          </div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img
+              src="/static/images/2020robot.png"
+              alt="L"
+              className="w-full"
+            />
+            <div className="text-center px-6 py-4 font-bold  text-xl mb-2">
+              Robot
+            </div>
+          </div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img
+              src="/static/images/2020robot.png"
+              alt="L"
+              className="w-full"
+            />
+            <div className="text-center px-6 py-4 font-bold  text-xl mb-2">
+              Robot
+            </div>
+          </div>
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img
+              src="/static/images/2020robot.png"
+              alt="L"
+              className="w-full"
+            />
+            <div className="text-center px-6 py-4 font-bold  text-xl mb-2">
+              Robot
+            </div>
+            
+            
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
 
