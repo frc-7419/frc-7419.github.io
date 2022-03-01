cfd2929de69519dfa0abc8f3df2688a49c686f14
poluk sharma <poluk.sharma@gmail.com>
2022-03-01 11:25:19 -0800

    init

--- partials/Header.js
+++ partials/Header.js
@@ -87,6 +87,7 @@ function Header() {
                   <li className="font-medium text-gray-600 hover:text-gray-800 px-5 py-3 transition duration-150 ease-in-out"><Link href="/about/leadership">{/**/}<a>Leadership</a></Link></li>
                   <li className="font-medium text-gray-600 hover:text-gray-800 px-5 py-3 transition duration-150 ease-in-out"><Link href="/about/sponsors">{/**/}<a>Sponsors</a></Link></li>
                   <li className="font-medium text-gray-600 hover:text-gray-800 px-5 py-3 transition duration-150 ease-in-out"><Link href="/about/contact">{/**/}<a>Contact Us</a></Link></li>
+                  <li className="font-medium text-gray-600 hover:text-gray-800 px-5 py-3 transition duration-150 ease-in-out"><Link href="/about/business">{/**/}<a>Business Plan</a></Link></li>
                 </Dropdown>
               {/* </li> */}
               <li>
