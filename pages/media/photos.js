a4d4b70b6a03381dc435bad1a4623e6a0a9e044c
Stephen Xie <49465499+StephenXie@users.noreply.github.com>
2022-02-28 19:51:45 -0800

    idk why im committing this its just reformatting

--- pages/media/photos.js
+++ pages/media/photos.js
@@ -1,35 +1,32 @@
-import React, { useState } from 'react';
-import Photo from '../../partials/Photo';
-import ReactPlayer from 'react-player'
-  function Photos() {
-  
-  
-  
+import React, { useState } from "react";
+import Photo from "../../partials/Photo";
+import ReactPlayer from "react-player";
+function Photos() {
   return (
     <main className="flex-grow">
-        <section className="relative">
-          <div className="max-w-6xl mx-auto px-4 sm:px-6">
-            <div className="py-12 md:py-20">
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="py-12 md:py-20">
+            {/* Section header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+              <h2 className="h2 mb-4">Photos</h2>
+              <p className="text-xl text-gray-600" data-aos="zoom-y-out">
+                A collection of media that represents our team
+              </p>
+            </div>
+          </div>
+        </div>
 
-              {/* Section header */}
-              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-                <h2 className="h2 mb-4">Photos</h2>
-                <p className="text-xl text-gray-600" data-aos="zoom-y-out">A collection of media that represents our team</p>
-              </div>
-              </div>
-              </div>
-
-              {/* Items */}
-          <div className="max-w-sm md:max-w-7xl mx-auto grid grid-cols-3 gap-4 overflow-visible">
-            <Photo link="https://drive.google.com/uc?id=1Vo7nFYwTWajHXoYMPRXdfYE_hkrZjnyt&export=download" caption="wow"/>
-            <Photo link="/static/images/2020robot.png" caption="Robot"/>
-          </div> 
-
-          
-             
-        </section>
-      </main>
-
+        {/* Items */}
+        <div className="max-w-sm md:max-w-7xl mx-auto grid grid-cols-3 gap-4 overflow-visible">
+          <Photo
+            link="https://drive.google.com/uc?id=1Vo7nFYwTWajHXoYMPRXdfYE_hkrZjnyt&export=download"
+            caption="wow"
+          />
+          <Photo link="/static/images/2020robot.png" caption="Robot" />
+        </div>
+      </section>
+    </main>
   );
 }
 
