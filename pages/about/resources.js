4f3dc448fc243991eec6b8098cff25b93760e217
Siddharth Srinivasan <sidsrinivasan6@gmail.com>
2022-03-08 14:27:35 -0800

    Update resources.js

--- pages/about/resources.js
+++ pages/about/resources.js
@@ -15,43 +15,56 @@ function Resources() {
               </p>
             </div>
             {/* 7419 Resources */}
-            <h4 className="h4 mb-1">7419 Resources</h4>
+            <h4 className="h4 mb-2">7419 Resources</h4>
            
               <a
                 href="https://github.com/frc-7419"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
               >
                 Github
               </a>
               <a
                 href="https://github.com/frc-7419"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
               >
                 Github
               </a>
               <a
                 href="https://github.com/frc-7419"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
               >
                 Github
               </a>
               <a
                 href="https://github.com/frc-7419"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
               >
                 Github
               </a>
               <a
                 href="https://github.com/frc-7419"
-                className="font-medium text-blue-600 hover:text-blue-800  md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-medium text-blue-600 hover:text-blue-800  md:px-5 px-2 pb-4 pt-1 flex items-center transition duration-150 ease-in-out"
               >
                 Github
               </a>
+              <hr></hr>
                {/* Competition */}
-            <h4 className="h4 mb-1">7419 Resources</h4>
+            <h4 className="h4 mb-2 pt-4">Competition Resources</h4>
             <div>
               <a
                 href="https://firstfrc.blob.core.windows.net/frc2022/Manual/2022FRCGameManual.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
               >
                 Rapid React Manual
