4c45adb941bd02a9fc8217874061cf308afa5969
3b1b <11601040+3b1b@users.noreply.github.com>
2022-03-08 14:26:16 -0800

    add video componenet

---
+++ partials/Video.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Video = (link) => {
+  return (
+    <div className="videoWrapper max-w-lg rounded shadow-lg ">
+      <iframe
+        src={link}
+        height="100%"
+        width="100%"
+        allow="autoplay"
+        allowFullScreen={true}
+      ></iframe>
+    </div>
+  );
+};
+
+export default Video;
