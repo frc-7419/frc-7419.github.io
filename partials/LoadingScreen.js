ff01ad3607de1467b9f298d45d7f2d383b5acd3f
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-03-08 22:06:43 -0800

    happy joey

--- partials/LoadingScreen.js
+++ partials/LoadingScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GoodRobot2020 from '../public/static/images/GoodRobot2020.js';
-
+import Transition from "../utils/Transition.js";
 const LoadingScreen = () => {
   return (
-    <div className="mx-auto">
+    <div className="opacity-25 fixed inset-0 z-40 bg-black">
+    <div className="absolute top-1/2 left-1/2 z-50 transform -translate-x-1/2 -translate-y-1/2">
         <GoodRobot2020 className="animate-spin" size="16rem" color="#ADD8E6" autoSpin={true}/>
     </div>
-  );
+    </div>
+    );
 };
 
 export default LoadingScreen;
