9d9c4e7a3b560e9726689953f1aa8e52d5c52934
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-03-08 21:45:27 -0800

    FINALLY

--- pages/media/videos.js
+++ pages/media/videos.js
@@ -1,25 +1,23 @@
 import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
-import GoodRobot2020 from '../../public/static/images/GoodRobot2020.js';
+import GoodRobot2020 from "../../public/static/images/GoodRobot2020.js";
 import Video from "../../partials/Video";
 import SoftwareTesting from "../../public/static/links/media/SoftwareTesting";
 import LoadingScreen from "../../partials/LoadingScreen.js";
 function Videos() {
-  
   const [isLoading, setIsLoading] = useState(true);
   const [numberOfVideosLoaded, setNumberOfVideosLoaded] = useState(0);
   const SoftwareTestingVideos = SoftwareTesting.SoftwareTesting;
   const TotalVideos = SoftwareTestingVideos.length;
-  const updateLoad = () => {
+
+  useEffect(() => {
     console.log(numberOfVideosLoaded);
-    setNumberOfVideosLoaded(numberOfVideosLoaded+1);
     if (numberOfVideosLoaded >= TotalVideos) {
       setIsLoading(false);
     }
-  }
+  }, [numberOfVideosLoaded]);
   return (
     <main className="flex-grow">
-
       <section className="relative">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           <div className="py-12 md:py-20">
@@ -30,15 +28,26 @@ function Videos() {
                 A collection of media that represents our team
               </p>
             </div>
-            {isLoading && <LoadingScreen/>}
+            {isLoading && <LoadingScreen />}
           </div>
         </div>
 
         {/* Items */}
-        
+
         <div className="max-w-sm md:max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 overflow-x-scroll ">
-          {SoftwareTestingVideos.map((video) => <Video key={video.toString()} link={video} onLoading={setTimeout(()=>{updateLoad();}, 200)}/>)}
-          <Video link="https://www.youtube.com/embed/dQw4w9WgXcQ" onLoading={setTimeout(()=>{updateLoad();}, 200)}/>
+          {SoftwareTestingVideos.map((video) => (
+            <Video
+              key={video.toString()}
+              link={video}
+              onLoading={() =>
+                setNumberOfVideosLoaded(numberOfVideosLoaded + 1)
+              }
+            />
+          ))}
+          <Video
+            link="https://www.youtube.com/embed/dQw4w9WgXcQ"
+            onLoading={() => setNumberOfVideosLoaded(numberOfVideosLoaded + 1)}
+          />
           {/* <div className="videoWrapper max-w-lg rounded shadow-lg ">
             <iframe
               src="https://drive.google.com/file/d/1Pp1uf2EfpJY_UaxEPM2c5FHC26ljEvN1/preview"
