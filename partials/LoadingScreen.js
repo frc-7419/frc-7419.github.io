8801f25d5900968535c4b3d76448d5785eb04245
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-03-08 23:02:33 -0800

    fixed instead of absolute

--- partials/LoadingScreen.js
+++ partials/LoadingScreen.js
@@ -7,7 +7,7 @@ const LoadingScreen = () => {
     <div className="opacity-50 fixed inset-0 z-40 bg-black">
     
     </div>
-    <div className="absolute top-1/2 left-1/2 z-40 transform -translate-x-1/2 -translate-y-1/2">
+    <div className="fixed top-1/2 left-1/2 z-40 transform -translate-x-1/2 -translate-y-1/2">
     {/* <GoodRobot2020 className="animate-spin" size="16rem" color="#FFFFFF" autoSpin={true}/> */}
     <img className=" animate-spin" src="/static/images/goodrobot2020.png"></img>
 </div>
