fb6aca10566f48a86ce732a668dde3dff817bf88
H0tP0cket <77374762+H0tP0cket@users.noreply.github.com>
2022-03-14 09:39:35 -0700

    initial resources page

---
+++ pages/about/resources.js
@@ -0,0 +1,95 @@
+import React from "react";
+
+function Resources() {
+  return (
+    <main>
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="py-12 md:py-20">
+            {/* Section header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+              <h2 className="h2 mb-4">Resources & Links</h2>
+              <p className="text-xl text-gray-600" data-aos="zoom-y-out">
+                Some Resources & Links to help you out
+              </p>
+            </div>
+            {/* 7419 Resources */}
+            <h4 className="h4 mb-2">7419 Resources</h4>
+
+            <a
+              href="https://github.com/frc-7419"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/frc-7419"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/frc-7419"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/frc-7419"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
+            >
+              Github
+            </a>
+            <a
+              href="https://github.com/frc-7419"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-medium text-blue-600 hover:text-blue-800  md:px-5 px-2 pb-4 pt-1 flex items-center transition duration-150 ease-in-out"
+            >
+              Github
+            </a>
+            <hr></hr>
+            {/* Competition */}
+            <h4 className="h4 mb-2 pt-4">Competition Resources</h4>
+            <div>
+              <a
+                href="https://firstfrc.blob.core.windows.net/frc2022/Manual/2022FRCGameManual.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
+              >
+                Rapid React Manual
+              </a>
+              <a
+                href="https://firstfrc.blob.core.windows.net/frc2022/Manual/2022FRCGameManual.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
+              >
+                Rapid React Manual
+              </a>
+              <a
+                href="https://firstfrc.blob.core.windows.net/frc2022/Manual/2022FRCGameManual.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="font-medium text-blue-600 hover:text-blue-800 md:px-5 px-2 py-1 flex items-center transition duration-150 ease-in-out"
+              >
+                Rapid React Field
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Resources;
