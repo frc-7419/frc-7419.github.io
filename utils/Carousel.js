0e1ad2fe66583323711ba5b6044bcfddf9925d7c
pryme-svg <edoc.www@gmail.com>
2022-03-15 10:34:31 -0700

    add robot page

--- utils/Carousel.js
+++ utils/Carousel.js
@@ -1,37 +1,39 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
+
 function Carousel({ images }) {
   const [idx, setIdx] = useState(0);
 
   const next = () => {
     console.log(idx);
-    setIdx(idx == images.length - 1 ? 0 : idx + 1);
+    setIdx((idx == images.length - 1 ? 0 : (idx + 1)));
   };
   const previous = () => {
     console.log(idx);
-    setIdx(idx == 0 ? images.length - 1 : idx - 1);
+    setIdx((idx == 0 ? images.length - 1 : (idx - 1)));
   };
 
   return (
     <div className="mx-auto flex flex-row items-center">
       <button
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        className="mx-auto bg-transparent mr-2 hover:bg-blue-500 md-2 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
         onClick={previous}
       >
         {"<"}
       </button>
       <img
-        className="mx-auto rounded-lg"
+        className="mx-auto max-w-md   rounded-lg"
         alt=""
-        style={{
+        style={Object.assign({},(images[idx][1] != 0? {
           width: images[idx][1],
+        } : {}) ,(images[idx][2] != 0? {
           height: images[idx][2],
-        }}
+        } : {}))}
         src={images[idx][0]}
       />
       <button
-        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        className="mx-auto bg-transparent ml-2 hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
         onClick={next}
       >
         {">"}
