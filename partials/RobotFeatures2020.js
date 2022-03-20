357b63ec8fcc24216065a418c0b7b693bc576a05
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-03-20 14:26:35 -0700

    delete gray line

--- partials/RobotFeatures2020.js
+++ partials/RobotFeatures2020.js
@@ -105,8 +105,6 @@ function RobotFeatures2020() {
         className="absolute inset-0 bg-white pointer-events-none mb-16"
         aria-hidden="true"
       ></div>
-      <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
-
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-12 md:pt-20">
           {/* Section content */}
