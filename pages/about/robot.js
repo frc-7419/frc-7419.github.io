a948b285c85826f1dc7ecaebb2286134f7eac786
StephenXie <49465499+StephenXie@users.noreply.github.com>
2022-04-25 21:28:32 -0700

    fix text bug

--- pages/about/robot.js
+++ pages/about/robot.js
@@ -19,10 +19,9 @@ function Robot() {
             </p>
           </div>
           {/* 2022 Robot */}
-          <p className="text-xl" data-aos="zoom-y-out">
+          <p className="text-xl">
             <h4 className="mb-3 h4 mx-auto text-center">2022 Robot </h4>
-          </p>
-          <p className="max-w-4xl w-full mx-auto text-gray-800">
+            <p className="text-base max-w-4xl w-full mx-auto text-gray-800">
             Dubbed “Everybot II”, our 2020 robot was made to accomplish the
             goals of the FIRST’s challenge: INFINITE RECHARGE. The simple yet
             effective robot features an intake that feeds cargo into the
@@ -30,12 +29,13 @@ function Robot() {
             season, we implement the robot as an exemplar to test out new code
             and give new members an overview of the FRC program.
           </p>
+          </p>
+          
           <RobotFeatures2022 />
           {/* 2020 Robot */}
-          <p className="text-xl mt-10" data-aos="zoom-y-out">
+          <p className="text-xl mt-10">
             <h4 className="mb-3 h4 mx-auto text-center">2020 Robot </h4>
-          </p>
-          <p className="max-w-4xl w-full  mx-auto text-gray-800">
+            <p className="max-w-4xl text-base w-full  mx-auto text-gray-800">
             Dubbed “Everybot II”, our 2020 robot was made to accomplish the
             goals of the FIRST’s challenge: INFINITE RECHARGE. The simple yet
             effective robot features an intake that feeds cargo into the
@@ -43,6 +43,8 @@ function Robot() {
             season, we implement the robot as an exemplar to test out new code
             and give new members an overview of the FRC program.
           </p>
+          </p>
+          
           <RobotFeatures2020 />
         </div>
       </section>
