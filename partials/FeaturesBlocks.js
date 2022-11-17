1cfa8babc0cb2fe5a56c9d3c428c56f7caffa75f
wlmchen <wlmchen16@gmail.com>
2022-11-17 14:15:15 -0800

    fix recent news

--- partials/FeaturesBlocks.js
+++ partials/FeaturesBlocks.js
@@ -18,30 +18,30 @@ function FeaturesBlocks() {
       {/* <div className="absolute left-0 right-0 bottom-0 m-auto w-px p-px h-20 bg-gray-200 transform translate-y-1/2"></div> */}
 
       <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-16">
+        <div className="py-12 md:py-16 px-12">
           {/* Section header */}
-          <div className="md:pb-20 -ml-14 mb-24 flex flex-row space-x-10">
-          <img
-            className = "relative float-left rounded"
-            width = "40%"
-            src="/static/images/roboblur.png"
-            alt="Hero"
-          />
-          <div className="flex flex-col">
-          <h1
+          <div className="md:pb-20 -ml-14 mb-24 flex flex-col md:flex-row items-center space-x-10">
+            <img
+              className="relative float-left rounded flex-1"
+              width="40%"
+              src="/static/images/roboblur.png"
+              alt="Hero"
+            />
+            <div className="flex flex-[2_2_0%] flex-col">
+              <h1
                 className="text-3xl  px-2 md:text-4xl text-black font-extrabold leading-tighter tracking-tighter mb-4"
                 data-aos="zoom-y-out"
               >
                 Recent News
               </h1>
-            <p className="text-xl text-gray-600 item-body px-2 ">
-            We’re so happy to have qualified for the FRC World Championships after being part of the winning alliance at the Silicon Valley Regional! Along with our alliance partners, Team 254, the Cheesy Poofs and the Team 1678, Citrus Circuits, we set a new world record of 179 points!</p>
+              <p className="text-xl text-gray-600 item-body px-2 ">
+                We’re so happy to have qualified for the FRC World Championships after being part of the winning alliance at the Silicon Valley Regional! Along with our alliance partners, Team 254, the Cheesy Poofs and the Team 1678, Citrus Circuits, we set a new world record of 179 points!</p>
             </div>
           </div>
           {/* Items */}
           <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
             {/* 1st item */}
-            
+
             <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
               <FaHandshake className="h-16 w-16 p-1 -mt-1 mb-2 text-blue-600 hover:animate-spin" />
               <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">
