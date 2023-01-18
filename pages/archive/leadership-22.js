cd20e30bd6fbef19f2308933deb4f75b9aedda7f
StephenXie <49465499+StephenXie@users.noreply.github.com>
2023-01-18 09:47:06 -0800

    Update leadership(incomplete)

---
+++ pages/archive/leadership-22.js
@@ -0,0 +1,180 @@
+function Leadership() {
+  const team = [
+    {
+      name: "Stephen Xie",
+      position: "Programming",
+      year: "24",
+      img: "/static/images/Leadership/StephenXieHeadShot.jpg",
+    },
+    {
+      name: "Karan Saini",
+      position: "Programming",
+      year: "22",
+      img: "/static/images/Leadership/KaranSainiHeadshot.png",
+    },
+    {
+      name: "Dhruv Bisla",
+      position: "Mechanical",
+      year: "24",
+      img: "/static/images/Leadership/DhruvBislaHeadshot.png",
+    },
+    {
+      name: "Charles Jin",
+      position: "Programming",
+      year: "23",
+      img: "/static/images/Leadership/charlesJinheadshot.jpg",
+    },
+    {
+      name: "Krithik Duvvuri",
+      position: "Mechanical",
+      year: "24",
+      img: "/static/images/Leadership/KrithikDuvvuriHeadshot.jpg",
+    },
+    {
+      name: "Brigitte Chung",
+      position: "Non-technical",
+      year: "22",
+      img: "/static/images/Leadership/brigitte_headshot.png",
+    },
+    {
+      name: "Aarav Sharma",
+      position: "Programming",
+      year: "23",
+      img: "/static/images/Leadership/SharmaNew.jpg",
+    },
+    {
+      name: "Aarav Wattal",
+      position: "Programming",
+      year: "23",
+      img: "/static/images/Leadership/AaravWattalHeadshot.jpg",
+    },
+    {
+      name: "Mimansa Kant",
+      position: "Non-technical",
+      year: "23",
+      img: "/static/images/Leadership/Screenshot_20211025-173212_Gallery.jpg",
+    },
+    {
+      name: "Modakar Kurma",
+      position: "Mechanical",
+      year: "23",
+      img: "/static/images/Leadership/ModaKurmaHeadshot.jpg",
+    },
+    {
+      name: "Rohan Kumar",
+      position: "Mechanical",
+      year: "22",
+      img: "/static/images/Leadership/RohanKumarHeadshot.png",
+    },
+    {
+      name: "Poluk Sharma",
+      position: "Programming",
+      year: "24",
+      img: "/static/images/Leadership/Poluk)_Headshot.jpeg",
+    },
+    {
+      name: "Ryan Biggee",
+      position: "Mechanical",
+      year: "25",
+      img: "/static/images/Leadership/RyanB.jpg",
+    },
+    {
+      name: "Sarp Gursel",
+      position: "Mechanical",
+      year: "23",
+      img: "/static/images/Leadership/Sarp Gursel Headshot.JPG",
+    },
+    {
+      name: "Sneha Gadekarla",
+      position: "Programming",
+      year: "24",
+      img: "/static/images/Leadership/SnehaGadekarlaHeadshot.jpg",
+    },
+    {
+      name: "Sudharsan Gopalakrishnan",
+      position: "Programming",
+      year: "23",
+      img: "/static/images/Leadership/SudharsanGopalakrishnanHeadshot.png",
+    },
+    {
+      name: "Mark Yang",
+      position: "Mechanical",
+      year: "22",
+      img: "/static/images/Leadership/Mark.jpg",
+    },
+    {
+      name: "Nancy Wu",
+      position: "Mechanical",
+      year: "23",
+      img: "/static/images/Leadership/Nancy Wu Headshot.jpeg",
+    },
+    {
+      name: "Vivi Cheng",
+      position: "Programming",
+      year: "23",
+      img: "/static/images/Leadership/ViviChengHeadshot.jpg",
+    },
+  ];
+
+  return (
+    <main className="flex-grow">
+      <section className="relative">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6">
+          <div className="py-12 md:py-20">
+            {/* Section header */}
+            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-14">
+              <h2 className="h2">2021-2022 Student Leadership</h2>
+              <p className="text-lg text-gray-600 mt-10" data-aos="zoom-y-out">
+                True to the FIRST® mission of sparking invention and creativity,
+                team operations are entirely student-led. We have three main
+                departments: Mechanical, Programming, and Non-Technical. In each
+                of these sub-teams, leadership positions are earned through
+                continuous effort and dedication. We take pride in our team’s
+                culture that helps our leaders further explore their own skills
+                and passions and find joy in helping others.
+              </p>
+            </div>
+
+            {/* Items */}
+            <div className="my-5 mx-auto">
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+                {team.map((member, index) => {
+                  // fix id
+                  return (
+                    <div
+                      key={index}
+                      className="flex space-x-6"
+                      style={{ marginLeft: "25%" }}
+                    >
+                      <img
+                        src={member.img}
+                        className="h-16 object-cover w-16 rounded-xl bg-gray-800 border-none shadow-sm"
+                        alt={member.name}
+                      />
+                      <div className="block text-lg">
+                        <a
+                          className="hover:text-gray-700"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <p className="font-semibold break-normal">
+                            {member.name} '{member.year}
+                          </p>
+                        </a>
+                        <p className="mt-1 text-base inline text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-800 font-medium">
+                          {member.position}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Leadership;
